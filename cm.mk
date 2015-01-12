9893ee022d984a03dc2777c613b695d6f4a2723c
abun880007 <abun880007@team-ub.co.uk>
2015-01-12 14:25:16 +0000

    fix apn

--- cm.mk
+++ cm.mk
@@ -5,6 +5,9 @@ PRODUCT_RELEASE_NAME := t0lte
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
 
